3c998deec98af3a4a754fa0f1306fee2308430f4
jaquelinecordeiiro <jaquelinecordeiro01@gmail.com>
2022-08-31 17:32:49 -0300

    código pronto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite seu nome");
+    String nomeCliente = scanner.next(); 
+    
+    System.out.println("Digite sua agência");
+    String agencia = scanner.next(); 
+
+    System.out.println("Digite número da agência");
+    int numero = scanner.nextInt(); 
+       
+    System.out.println("Digite seu saldo");
+    double saldo = scanner.nextDouble();
+
+    System.out.println(" Olá " + nomeCliente + " , " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " ,conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+
     }
 }
